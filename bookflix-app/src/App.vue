99a7bc9ee9986bbb2b276d999c5b262495315aa4
lucas raga <lucasraga29@gmail.com>
2024-04-25 17:43:19 +0200

    Diseño inicial de la home lista

--- bookflix-app/src/App.vue
+++ bookflix-app/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <nav>
-      <router-link to="/" class="text-2xl">PATATA</router-link> |
-      <router-link to="/about" class="m-10 text-green-900 text-xl">ABOUT</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="sass">
+<script>
+</script>
 
+<style lang="scss">
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- bookflix-app/src/main.js
+++ bookflix-app/src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './input.css';
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBars)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(store).use(router)
+
+app.mount('#app')
