a11413aa9b9cf043754f93c6350492a032f63baa
Lucas Raga <155363044+LucasRagaLara@users.noreply.github.com>
2024-05-29 01:19:03 +0200

    Update vue.config.js for deploy

--- bookflix-app/vue.config.js
+++ bookflix-app/vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://bookflix-app.netlify.app',
         changeOrigin: true,
         // pathRewrite: {'^/api': ''}
       }
