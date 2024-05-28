32ed31b977845cda182b19dbc341a1620ee7db9b
Lucas Raga <155363044+LucasRagaLara@users.noreply.github.com>
2024-05-29 01:38:29 +0200

    Update server.js deploy 01

--- backend-bookflix/server.js
+++ backend-bookflix/server.js
@@ -38,11 +38,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../bookflix-app/dist', 'index.html'));
 });
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost'
-const port = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL
+// const port = process.env.PORT || 3000;
 
 // Inicio del servidor
-app.listen(port, () => {
-    console.log(`Servidor en marcha en la ruta: ${BASE_URL}:${port}`);
+app.listen(() => {
+    console.log(`Servidor en marcha en la ruta: ${BASE_URL}`);
     console.log(app.use(serveStatic(path.join(__dirname, '../bookflix-app/dist'))));
 });
