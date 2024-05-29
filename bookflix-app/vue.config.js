dfcf02212e11689b90a50dcea2037107a0756305
Lucas Raga <lucasraga29@gmail.com>
2024-05-29 18:14:23 +0200

    reinicio de rama main para local

--- bookflix-app/vue.config.js
+++ bookflix-app/vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'https://bookflix-backend.onrender.com' || 'http://localhost:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         // pathRewrite: {'^/api': ''}
       }
